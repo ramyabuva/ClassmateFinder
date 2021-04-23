3e8114fd3129125f5a9d6823f845c713bd8c502a
Ramya B <rsb4zm@virginia.edu>
2021-04-23 11:52:29 -0700

    Add login and sign up functionality

--- main.py
+++ main.py
@@ -7,23 +7,21 @@ import xml.etree.ElementTree as ET
 
 from collections import defaultdict
 
-from flask import Flask, request, redirect, flash, url_for
+from flask import Flask, request, redirect, flash, url_for, session
 from flask import render_template
 
 
 from pagination import Pagination
 import mysql.connector
 
+import hashlib
+
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 
 app = Flask(__name__)
+app.secret_key = 'randomstring'
 
-
-
-cnx = mysql.connector.connect(host='usersrv01.cs.virginia.edu', user='rsb4zm', password='Spr1ng2021!!',
-                              database='rsb4zm_classmatefinder', auth_plugin='mysql_native_password')
-mycursor = cnx.cursor()
 
 # mycursor.execute("SELECT * FROM account")
 
@@ -34,8 +32,54 @@ mycursor = cnx.cursor()
 
 @app.route('/', methods=['GET'])
 def main():
-	return render_template('index.html')
+	if 'user' in session:
+		return render_template('profile.html')
+	return render_template('login.html')
 
+@app.route('/logout', methods=['GET'])
+def logout():
+	if 'user' in session:
+		session.pop('user', None)
+	return redirect('/')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == "POST":
+		cnx = mysql.connector.connect(host='usersrv01.cs.virginia.edu', user='rsb4zm', password='Spr1ng2021!!',
+                              database='rsb4zm_classmatefinder', auth_plugin='mysql_native_password')
+		mycursor = cnx.cursor()
+		query = "SELECT * FROM User WHERE comp_id = '{cid}'".format(cid = request.form['cid-login'].lower())
+		mycursor.execute(query)
+		myresult = mycursor.fetchall()
+		cnx.close()
+		if len(myresult) != 0 and myresult[0][5] == request.form['password']:
+			session['user'] = request.form['cid-login'].lower()
+			return render_template('profile.html') 
+		return render_template('login.html') # TODO: figure out message for incorrect username/password
+	return render_template('login.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+	session['user'] = request.form['cid'].lower()
+	vals = "('{cid}', '{fname}',{gradyear},'{lname}','{major}','{pwd}')".format(
+		cid = request.form['cid'].lower(),
+		fname = request.form['firstname'],
+		gradyear = request.form['grad-year'],
+		lname = request.form['lastname'],
+		major = request.form['major'],
+		# TODO: hash passwords and store them in database
+		pwd = request.form['password'] #hashlib.pbkdf2_hmac('sha256', request.form['password'].encode('utf-8'), bytes([32]), 100000)
+		)
+	cnx = mysql.connector.connect(host='usersrv01.cs.virginia.edu', user='rsb4zm', password='Spr1ng2021!!',
+                              database='rsb4zm_classmatefinder', auth_plugin='mysql_native_password')
+	mycursor = cnx.cursor()
+	# TODO: figure out why insertion isn't working
+	query = "INSERT INTO `User` (`comp_id`, `first_name`, `graduation_year`, `last_name`, `major`, `password`) VALUES " + vals
+	mycursor.execute(query)
+	cnx.commit()
+	cnx.close()
+	return render_template('profile.html')
 ####################################################
 
 # *****************************
@@ -47,7 +91,9 @@ import os, sys, logging, traceback, codecs, datetime, copy, time, ast, math, re,
 
 
 @app.route('/classmatefinder', methods = ["GET", "POST"])
-def classmatefinder():
+def classmatefinder(): 
+	if 'user' not in session:
+		return redirect('/')
 	# If the request is GET then only render template.
 	if request.method == "GET":
 		return render_template('classmatefinder.html')
@@ -56,6 +102,8 @@ def classmatefinder():
 
 @app.route('/friends', methods = ["GET", "POST"])
 def friends():
+	if 'user' not in session:
+		return redirect('/')
 	# If the request is GET then only render template.
 	if request.method == "GET":
 		return render_template('friends.html')
@@ -64,6 +112,8 @@ def friends():
 
 @app.route('/clubs', methods = ["GET", "POST"])
 def clubs():
+	if 'user' not in session:
+		return redirect('/')
 	# If the request is GET then only render template.
 	if request.method == "GET":
 		return render_template('clubs.html')
